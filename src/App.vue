1a88e10d47e5f205384ddcb1d7affbee05793939
perryLinke <778520330@qq.com>
2025-02-17 17:30:57 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,25 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { onMounted } from 'vue';
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-let width, height, scene, camera, renderer, controls, model, skeleton, mixer
+let container, width, height; //threejs容器、宽、高
+let scene, camera, renderer, controls, clock;//场景、相机、渲染器、轨道控制器、时钟
+let model, skeleton, mixer;
 let idleAction, walkAction, runAction;
 let idleWeight, walkWeight, runWeight;
 let actions, settings;
-
+let keyStates = {}; //存储按键信息
 function init() {
-  let container = document.getElementById('threeContainer')
+  container = document.getElementById('threeContainer')
   // console.dir(container);
   width = container.offsetWidth
   height = container.offsetHeight
 
   camera = new THREE.PerspectiveCamera(45, width / height, 1, 100);
+  camera.rotation.order = 'YXZ';
   camera.position.set(1, 2, - 3);
   camera.lookAt(0, 1, 0);
 
-  // clock = new THREE.Clock();
+  clock = new THREE.Clock();
   // 场景
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xa0a0a0);
@@ -51,10 +54,11 @@ function init() {
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(width, height);
+  // renderer.setAnimationLoop(animate);
   renderer.shadowMap.enabled = true;
   container.appendChild(renderer.domElement);
   // 添加坐标系
-  const axesHelper = new THREE.AxesHelper(20); // 参数为坐标轴的长度
+  const axesHelper = new THREE.AxesHelper(2); // 参数为坐标轴的长度
   scene.add(axesHelper);
 
   controls = new OrbitControls(camera, renderer.domElement);
@@ -96,23 +100,90 @@ function init() {
     runAction = mixer.clipAction(animations[1]);
 
     actions = [idleAction, walkAction, runAction];
+    setWeight(idleAction, 1.0);
+    setWeight(walkAction, 0.0);
+    setWeight(runAction, 0.0);
 
+    actions.forEach(function (action) {
+      action.play();
+    });
+
+    // runAction.play()
+    renderer.setAnimationLoop(animate);
     // activateAllActions();
-
-    // animate();
 
   });
 }
-function animate() {
-  requestAnimationFrame(animate);
-  controls.update();
-  renderer.render(scene, camera);
+let keyCodeSet = new Set(['KeyW', 'KeyA', 'KeyS', 'KeyD', 'ShiftLeft'])
+function eventListener() {
+  document.addEventListener('keydown', (event) => {
+    if (keyCodeSet.has(event.code)) {
+      keyStates[event.code] = true;
+    }
+  });
+  document.addEventListener('keyup', (event) => {
+    if (keyCodeSet.has(event.code)) {
+      keyStates[event.code] = false;
+    }
+  });
+  // container.addEventListener('mousedown', () => {
+  //   document.body.requestPointerLock();
+  //   // mouseTime = performance.now();
+  // });
+  // document.addEventListener('mouseup', () => {
+  //   // if (document.pointerLockElement !== null) throwBall();
+  // });
+  // document.body.addEventListener('mousemove', (event) => {
+  //   if (document.pointerLockElement === document.body) {
+  //     camera.rotation.y -= event.movementX / 1500;
+  //     camera.rotation.x -= event.movementY / 1500;
+  //   }
+  // });
 }
 
+const STATE = {
+  IDLE: 'IDLE', // 停止状态
+  WALK: 'WALK', // 走动状态
+  IDLE_TO_WALK: 'IDLE_TO_WALK', // 停止过渡到走动
+  WALK_TO_IDLE: 'WALK_TO_IDLE', // 走动过渡到停止
+};
+
+function keyStatesControls() {
+  // console.log(
+  //   keyStates
+  // );
+  if (keyStates['KeyW']) {
+  }
+  if (keyStates['KeyA']) {
+  }
+  if (keyStates['KeyS']) {
+  }
+  if (keyStates['KeyD']) {
+  }
+  if (keyStates['ShiftLeft']) {
+  }
+}
+
+function setWeight(action, weight) {
+  action.enabled = true;
+  action.setEffectiveTimeScale(1);
+  action.setEffectiveWeight(weight);
+}
+
+function animate() {
+  let mixerUpdateDelta = clock.getDelta();
+  if (mixer) {
+    mixer.update(mixerUpdateDelta);
+  }
+  controls.update();
+  renderer.render(scene, camera);
+  keyStatesControls()
+}
 
 onMounted(() => {
   init()
-  animate()
+  // animate()
+  eventListener()
 })
 </script>
 
